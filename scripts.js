cc37d0c0af0421f116a6c49653d93f8c7e49a663
Manoranjan Kumar <manoranjan.kumar@outlook.com>
2022-11-29 19:01:32 +0530

    Use DOM manipulation and add basic UI

--- scripts.js
+++ scripts.js
@@ -1,99 +1,70 @@
-function getComputerChoice() { // Randomly returns "Rock", "Paper", or "Scissors"
+let playerScore = 0;
+let computerScore = 0;
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function getComputerChoice() { // Randomly returns "rock", "paper", or "scissors"
+    let choices = ["rock", "paper", "scissors"];
     let min = 1;
     let max = 3;
-    let num = Math.floor(Math.random() * (max - min + 1) + min);
+    return choices[(Math.floor(Math.random() * (max - min + 1) + min))-1];
+}
 
-    switch (num) {
-        case 1:
-            return "Rock";
-            break;
-        
-        case 2:
-            return "Paper";
-            break;
-        
-        case 3:
-            return "Scissors";
-            break;
-    }
+const buttons = document.querySelectorAll("button"); // This function calls playRound() with the right selection when any button is clicked.
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        playRound(button.className);
+    });
+});
+
+function disableButtons() {
+    buttons.forEach(button => {
+        button.disabled = true;
+    })
 }
 
 function playRound(playerChoice) { // Plays a round of RPS and returns the winner.
-    let whoWon; 
     let computerChoice = getComputerChoice();
-
-    playerChoice = playerChoice.slice(0,1).toUpperCase() + playerChoice.toLowerCase().slice(1);
-
-    while (playerChoice !== "Rock" && playerChoice !== "Paper" && playerChoice !== "Scissors") {
-        playerChoice = prompt("Invalid choice. Rock/Paper/Scissors?")
-        playerChoice = playerChoice.slice(0,1).toUpperCase() + playerChoice.toLowerCase().slice(1);
-    }
+    const scoreboard = document.querySelector(".scoreboard");
+    const results = document.querySelector(".results");
+    const roundResult = document.createElement("div");
+    const gameResult = document.createElement("div");
+    gameResult.setAttribute("style", "padding-top: 10px; font-size: 20px;");
 
     if (playerChoice === computerChoice) {
-        console.log(`It's a draw!`);
-        return whoWon = 0;
+        roundResult.textContent = "It's a draw!";
     }
 
-    else if (playerChoice === "Rock" && computerChoice === "Scissors" ||
-    playerChoice === "Paper" && computerChoice === "Rock" ||
-    playerChoice === "Scissors" && computerChoice === "Paper") {
-        console.log(`You win! ${playerChoice} beats ${computerChoice}!`);
-        return whoWon = 1;
+    else if (playerChoice === "rock" && computerChoice === "scissors" ||
+    playerChoice === "paper" && computerChoice === "rock" ||
+    playerChoice === "scissors" && computerChoice === "paper") {
+        playerScore++;
+        roundResult.textContent = `You win! ${capitalizeFirstLetter(playerChoice)} beats ${capitalizeFirstLetter(computerChoice)}!`;
     }
 
     else {
-        console.log(`You lose! ${computerChoice} beats ${playerChoice}!`);
-        return whoWon = 2;
+        computerScore++;
+        roundResult.textContent = `You lose! ${capitalizeFirstLetter(computerChoice)} beats ${capitalizeFirstLetter(playerChoice)}!`;
     }
+
+    results.appendChild(roundResult);
+    scoreboard.textContent = `Player Score: ${playerScore}\nComputer Score: ${computerScore}`;
+
+    if (playerScore === 5) {
+        gameResult.textContent = "You win the game!";
+        gameResult.style.color = "green";
+        results.appendChild(gameResult);
+        disableButtons();
+    }
+
+    if (computerScore === 5) {
+        gameResult.textContent = "The computer wins the game!";
+        gameResult.style.color = "red";
+        results.appendChild(gameResult);
+        disableButtons();
+    }
+
+    return;
 }
-
-function game() { // Plays five rounds of RPS.
-    let playerScore = 0;
-    let computerScore = 0;
-    let whoWon;
-
-    for (let i = 0; i < 5; i++) {
-        whoWon = playRound(prompt("Rock, Paper, or Scissors?"),getComputerChoice());
-
-        switch (whoWon) {
-            case 1:
-                playerScore++;
-                console.log(`Player score: ${playerScore}\nComputer score: ${computerScore}`);
-                break;
-            
-            case 2:
-                computerScore++;
-                console.log(`Player score: ${playerScore}\nComputer score: ${computerScore}`);
-                break;
-
-            default:
-                console.log(`Player score: ${playerScore}\nComputer score: ${computerScore}`);
-        }
-        
-        /*if (whoWon === 1) {
-            playerScore++;
-            console.log(`Player score: ${playerScore}\nComputer score: ${computerScore}`);
-        }
-
-        else if (whoWon === 2) {
-            computerScore++;
-            console.log(`Player score: ${playerScore}\nComputer score: ${computerScore}`);
-        }
-
-        else {
-            console.log(`Player score: ${playerScore}\nComputer score: ${computerScore}`);
-        }*/
-    }
-
-    if (playerScore === computerScore) {
-        console.log("It's a tie!");
-    }
-    else if (playerScore > computerScore) {
-        console.log(`You win! You won ${playerScore} out of 5 games.`);
-    }
-    else {
-        console.log(`You lose! You won ${playerScore} out of 5 games.`);
-    }
-}
-
-game();
